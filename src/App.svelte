e6244a5186c31a403d8d79a06a1103e715ada3d1
GrungeElFz <elf0114_@hotmail.com>
2023-03-18 23:25:32 -0500

    Added:    export default function `longpress(node, options)` (src/lib/actions/longpress.js)
              In the function `longpress(node, options)`
              - Added `console.log(node, options)`
              - Added return `update(newOptions)` and `destroy()`
    
    Modified:   Imported longpress from './lib/actions/longpress.js' (src/App.svelte)
                Defined a let a variable `showButton = true`
                Defined a let a variable `duration = 1000`
    
                Added a 'Toggle' type checkbox input button
                Added a 'Duration' type range input button
                Added a 'Test' button

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,23 @@
 <script>
+	import longpress from './lib/actions/longpress.js';
+
+	let showButton = true;
+	let duration = 1000;
 </script>
 
+<label>
+	<input type="checkbox" bind:checked={showButton} />
+	Toggle
+</label>
+
+<label>
+	<input type="range" bind:value={duration} max={4000} step={100}/>
+	{duration}ms
+</label>
+
+{#if showButton}
+	<button use:longpress={{ duration }}>Test</button>
+{/if}
 
 <style>
 </style>
